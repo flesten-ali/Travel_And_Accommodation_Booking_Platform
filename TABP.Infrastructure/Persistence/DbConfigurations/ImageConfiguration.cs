e4adf0f13ebe7285bd6dd4bdf5ed59e962c7861f
flesten-ali <fbada858@gmail.com>
2025-02-02 15:50:49 +0200

    update col types in the database

---
+++ TABP.Infrastructure/Persistence/DbConfigurations/ImageConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TABP.Domain.Entities;
+
+namespace TABP.Infrastructure.Persistence.DbConfigurations;
+public class ImageConfiguration : IEntityTypeConfiguration<Image>
+{
+    public void Configure(EntityTypeBuilder<Image> builder)
+    {
+        builder.Property(m => m.ImageableType).HasConversion<string>();
+    }
+}
---
+++ TABP.Infrastructure/Persistence/DbConfigurations/RoomClassConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TABP.Domain.Entities;
+
+namespace TABP.Infrastructure.Persistence.DbConfigurations;
+public class RoomClassConfiguration : IEntityTypeConfiguration<RoomClass>
+{
+    public void Configure(EntityTypeBuilder<RoomClass> builder)
+    {
+        builder.Property(r => r.RoomType).HasConversion<string>();
+
+    }
+}
