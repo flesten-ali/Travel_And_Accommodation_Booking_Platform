923893b9c262adae4d2522f98c0ba621e7514ec3
flesten-ali <fbada858@gmail.com>
2025-02-04 23:01:07 +0200

    create 'Add Amenity Command'

---
+++ TABP.Application/Amenities/Add/AddAmenityCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+namespace TABP.Application.Amenities.Add;
+
+public class AddAmenityCommand : IRequest<Guid>
+{
+    public string Name { get; set; }
+    public string? Description { get; set; }
+}
