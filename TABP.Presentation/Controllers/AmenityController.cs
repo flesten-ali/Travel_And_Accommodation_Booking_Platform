5adb5175e03f01c8599fc4ac3f92c13f2f585388
flesten-ali <fbada858@gmail.com>
2025-02-04 23:05:29 +0200

    add Amenity controller

---
+++ TABP.Presentation/Controllers/AmenityController.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TABP.Application.Amenities.Add;
+using TABP.Presentation.DTOs.Amenity;
+
+namespace TABP.Presentation.Controllers;
+[Route("api/amenities")]
+[ApiController]
+public class AmenityController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
+
+    public AmenityController(IMediator mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] AddAmenityRequest request)
+    {
+        var command = _mapper.Map<AddAmenityCommand>(request);
+        return Ok(await _mediator.Send(command));
+    }
+}
