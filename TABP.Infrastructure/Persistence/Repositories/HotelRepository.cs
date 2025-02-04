c99d5d292b402d9124f9d881e0e79d1112af583c
flesten-ali <fbada858@gmail.com>
2025-02-04 23:17:20 +0200

    Create hotel repo

---
+++ TABP.Infrastructure/Persistence/Repositories/HotelRepository.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq.Expressions;
+using TABP.Domain.Entities;
+using TABP.Domain.Interfaces.Persistence.Repositories;
+using TABP.Domain.Models;
+using TABP.Infrastructure.Extenstions;
+using TABP.Infrastructure.Persistence.DbContexts;
+namespace TABP.Infrastructure.Persistence.Repositories;
+
+public class HotelRepository(AppDbContext context) : Repository<Hotel>(context), IHotelRepository
+{
+    public async Task<PaginatedList<SearchHotelResult>> SearchHotels(
+        Expression<Func<Hotel, bool>> filter,
+        Func<IQueryable<Hotel>, IOrderedQueryable<Hotel>> orderBy,
+        int pageSize,
+        int pageNumber)
+    {
+        var hotels = DbSet.Where(filter);
+        hotels = orderBy(hotels);
+
+        var selectedResult = hotels.Select(h => new SearchHotelResult
+        {
+            Name = h.Name,
+            Description = h.Description,
+            StarRating = h.Rate,
+            PricePerNight = h.RoomClasses.Min(rc => rc.Price),
+            Thumbnail = context.Images.Where(img => img.Id == h.ThumbnailId && img.ImageableId == h.Id).FirstOrDefault()
+        });
+
+        var resultToReturn = selectedResult.GetRequestedPage(pageNumber, pageSize);
+        var paginationMetaData = await resultToReturn.GetPaginationMetaDataAsync(pageNumber, pageSize);
+        return new PaginatedList<SearchHotelResult>(resultToReturn, paginationMetaData);
+    }
+}
