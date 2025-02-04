18e47f0d450ffd20c1ec2b5d25cef1a07cd7db78
flesten-ali <fbada858@gmail.com>
2025-02-04 23:17:10 +0200

    Create city repo

---
+++ TABP.Infrastructure/Persistence/Repositories/CityRepository.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TABP.Domain.Entities;
+using TABP.Domain.Interfaces.Persistence.Repositories;
+using TABP.Infrastructure.Persistence.DbContexts;
+
+namespace TABP.Infrastructure.Persistence.Repositories;
+public class CityRepository(AppDbContext context) : Repository<City>(context), ICityRepository
+{
+ 
+}
