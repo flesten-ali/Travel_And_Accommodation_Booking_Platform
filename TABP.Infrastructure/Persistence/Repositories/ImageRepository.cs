2092240847cbc6614436bcab77703ae9cbd2c731
flesten-ali <fbada858@gmail.com>
2025-02-04 23:17:34 +0200

    Create image repo

---
+++ TABP.Infrastructure/Persistence/Repositories/ImageRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TABP.Domain.Entities;
+using TABP.Domain.Enums;
+using TABP.Domain.Interfaces.Persistence.Repositories;
+using TABP.Domain.Interfaces.Services;
+using TABP.Infrastructure.Persistence.DbContexts;
+
+namespace TABP.Infrastructure.Persistence.Repositories;
+public class ImageRepository : Repository<Image>, IImageRepository
+{
+    private readonly IImageStorageService _imageStorageService;
+
+    public ImageRepository(AppDbContext context, IImageStorageService imageStorageService) : base(context)
+    {
+        _imageStorageService = imageStorageService;
+    }
+
+    public async Task DeleteByIdAsync(Guid enittyId, ImageType imageType)
+    {
+        var images = await DbSet
+                .Where(img =>
+                        img.ImageableId == enittyId &&
+                        img.ImageType == imageType
+                )
+                .ToListAsync();
+
+        if (images.Count != 0)
+        {
+            foreach (var image in images)
+            {
+                await _imageStorageService.DeleteFileAsync(image.ImageUrl);
+                DbSet.Remove(image);
+            }
+        }
+    }
+}
