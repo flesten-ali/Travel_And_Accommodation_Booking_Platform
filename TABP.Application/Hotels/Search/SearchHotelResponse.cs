3e69ae02f89e0bc4891997ebae8bcc678bb568e4
flesten-ali <fbada858@gmail.com>
2025-02-04 23:07:26 +0200

    Create search Hotel response

---
+++ TABP.Application/Hotels/Search/SearchHotelResponse.cs
@@ -0,0 +1,15 @@
+﻿using TABP.Domain.Entities;
+
+namespace TABP.Application.Hotels.Search;
+
+public class SearchHotelResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public Image Thumbnail { get; set; }
+    public int StarRating { get; set; }
+    public double PricePerNight { get; set; }
+    public string Description { get; set; }
+}
+
+
