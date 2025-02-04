2734151eaf766c08d1e1ed699f5c1ecb9b5ef13b
flesten-ali <fbada858@gmail.com>
2025-02-04 23:33:43 +0200

    Add Image repo

---
+++ TABP.Domain/Interfaces/Persistence/Repositories/IImageRepository.cs
@@ -0,0 +1,8 @@
+﻿using TABP.Domain.Entities;
+using TABP.Domain.Enums;
+namespace TABP.Domain.Interfaces.Persistence.Repositories;
+
+public interface IImageRepository : IRepository<Image>
+{
+    Task DeleteByIdAsync(Guid entityId, ImageType imageType);
+}
