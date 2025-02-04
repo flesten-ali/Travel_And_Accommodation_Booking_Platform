ab988426c712e59ea0fe84f9ef7391c44207c127
flesten-ali <fbada858@gmail.com>
2025-02-04 23:17:48 +0200

    Create owner repo

---
+++ TABP.Infrastructure/Persistence/Repositories/OwnerRepository.cs
@@ -0,0 +1,8 @@
+﻿using TABP.Domain.Entities;
+using TABP.Domain.Interfaces.Persistence.Repositories;
+using TABP.Infrastructure.Persistence.DbContexts;
+
+namespace TABP.Infrastructure.Persistence.Repositories;
+public class OwnerRepository(AppDbContext context) : Repository<Owner>(context), IOwnerRepository
+{
+}
