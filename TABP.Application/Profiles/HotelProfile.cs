f26f7b6a90d6cb615700520f8a011565008e1a00
flesten-ali <fbada858@gmail.com>
2025-02-04 23:04:40 +0200

    update Hotel profile

---
+++ TABP.Application/Profiles/HotelProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using TABP.Application.Hotels.Add;
+using TABP.Application.Hotels.AddImageGallery;
+using TABP.Application.Hotels.AddThumbnail;
+using TABP.Application.Hotels.Search;
+using TABP.Domain.Entities;
+using TABP.Domain.Enums;
+using TABP.Domain.Models;
+namespace TABP.Application.Profiles;
+
+public class HotelProfile : Profile
+{
+    public HotelProfile()
+    {
+        CreateMap<SearchHotelResult, SearchHotelResponse>();
+
+        CreateMap<PaginatedList<SearchHotelResult>, PaginatedList<SearchHotelResponse>>();
+
+        CreateMap<AddHotelCommand, Hotel>();
+
+        CreateMap<AddThumbnailCommand, Image>()
+            .ForMember(dest => dest.ImageableId, opt => opt.MapFrom(src => src.HotelId))
+            .ForMember(dest => dest.ImageableType, opt => opt.MapFrom(src => ImageableType.Hotel));
+
+        CreateMap<AddImageGalleryCommand, Image>()
+        .ForMember(dest => dest.ImageableId, opt => opt.MapFrom(src => src.HotelId))
+        .ForMember(dest => dest.ImageableType, opt => opt.MapFrom(src => ImageableType.Hotel));
+    }
+}
