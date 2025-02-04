9b7cb5f8d019b1d0f7c3d768a136ef9f3fe17a48
flesten-ali <fbada858@gmail.com>
2025-02-04 23:46:15 +0200

    Add thumbnail request

---
+++ TABP.Presentation/DTOs/AddThumbnailRequest.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace TABP.Presentation.DTOs;
+
+public class AddThumbnailRequest
+{
+    public Guid HotelId { get; set; }
+    public IFormFile Thumbnail { get; set; }
+}
