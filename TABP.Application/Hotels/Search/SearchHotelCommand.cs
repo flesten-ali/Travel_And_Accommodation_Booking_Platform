f45ecc6ce476061f94febe4c9504bb2dfab96c5e
flesten-ali <fbada858@gmail.com>
2025-02-04 23:06:40 +0200

    Create search Hotel Handler command

---
+++ TABP.Application/Hotels/Search/SearchHotelCommand.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using TABP.Domain.Enums;
+using TABP.Domain.Models;
+namespace TABP.Application.Hotels.Search;
+
+public class SearchHotelCommand : IRequest<PaginatedList<SearchHotelResponse>>
+{
+    public string City { get; set; }
+    public DateTime? CheckInDate { get; set; }
+    public DateTime? CheckOutDate { get; set; }
+    public int ChildrenCapacity { get; set; }
+    public int AdultsCapacity { get; set; }
+    public int NumberOfRooms { get; set; }
+    public string? SortBy { get; set; }
+    public SortOrder SortOrder { get; set; } = SortOrder.Ascending;
+    public string? RoomType { get; set; }
+    public int? MinPrice { get; set; }
+    public int? MaxPrice { get; set; }
+    public int? StarRating { get; set; }
+    public int PageSize { get; set; } = 10;
+    public int PageNumber { get; set; } = 1;
+    public ICollection<string>? Amenities { get; set; } = [];
+}
