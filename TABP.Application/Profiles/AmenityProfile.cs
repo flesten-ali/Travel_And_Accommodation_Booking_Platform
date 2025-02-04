4b40959f30c7c90af491d97342395fe4cb9d82c3
flesten-ali <fbada858@gmail.com>
2025-02-04 23:03:15 +0200

    Add Amenity profile

---
+++ TABP.Application/Profiles/AmenityProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using TABP.Application.Amenities.Add;
+using TABP.Domain.Entities;
+
+namespace TABP.Application.Profiles;
+public class AmenityProfile  :Profile
+{
+    public AmenityProfile()
+    {
+        CreateMap<AddAmenityCommand, Amenity>();
+    }
+}
---
+++ TABP.Presentation/Profiles/AmenityProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using TABP.Application.Amenities.Add;
+using TABP.Presentation.DTOs.Amenity;
+
+namespace TABP.Presentation.Profiles;
+public class AmenityProfile : Profile
+{
+    public AmenityProfile()
+    {
+
+        CreateMap<AddAmenityRequest, AddAmenityCommand>();
+    }
+}
