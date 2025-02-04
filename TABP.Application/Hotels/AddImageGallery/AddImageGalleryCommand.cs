8d74f9b707e80250de5be6ca816766039762c3b8
flesten-ali <fbada858@gmail.com>
2025-02-04 23:08:18 +0200

    Create add Hotel image gallery command

---
+++ TABP.Application/Hotels/AddImageGallery/AddImageGalleryCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace TABP.Application.Hotels.AddImageGallery;
+public class AddImageGalleryCommand : IRequest<Guid>
+{
+    public Guid HotelId { get; set; }
+    public IFormFile Image { get; set; }
+}
