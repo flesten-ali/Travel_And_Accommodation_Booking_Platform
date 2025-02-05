ab19a96e6a340539155aafc8c64ffcf35fafc796
flesten-ali <fbada858@gmail.com>
2025-02-05 23:13:12 +0200

    Add Invoice Model

---
+++ TABP.Domain/Models/Invoice.cs
@@ -0,0 +1,14 @@
+﻿using TABP.Domain.Enums;
+
+namespace TABP.Domain.Models;
+public class Invoice
+{
+    public Guid InvoiceId { get; set; } = Guid.NewGuid();
+    public DateTime IssueDate { get; set; } = DateTime.Now;
+    public string HotelAddress { get; set; }
+    public double TotalPrice { get; set; }
+    public ICollection<RoomDetails> RoomDetails { get; set; }
+    public DateTime CheckInDate { get; set; }
+    public DateTime CheckOutDate { get; set; }
+    public PaymentStatus PaymentStatus { get; set; } = PaymentStatus.Pending;
+}
