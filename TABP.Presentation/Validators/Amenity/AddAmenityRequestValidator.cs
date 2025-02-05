09d3459406ddd9c511c3665ff0b24d85060ba281
flesten-ali <fbada858@gmail.com>
2025-02-05 14:06:58 +0200

    Add Amenity request validator

---
+++ TABP.Presentation/Validators/Amenity/AddAmenityRequestValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TABP.Presentation.DTOs.Amenity;
+
+namespace TABP.Presentation.Validators.Amenity;
+public class AddAmenityRequestValidator : AbstractValidator<AddAmenityRequest>
+{
+    public AddAmenityRequestValidator()
+    {
+        RuleFor(x=>x.Name).NotEmpty();
+    }
+}
