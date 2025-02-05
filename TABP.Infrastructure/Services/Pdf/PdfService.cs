5036c4d6020c9542cb81e66e260ea940ddbdaca2
flesten-ali <fbada858@gmail.com>
2025-02-05 23:18:23 +0200

    Add pdf service

---
+++ TABP.Infrastructure/Services/Pdf/PdfService.cs
@@ -0,0 +1,15 @@
+﻿using NReco.PdfGenerator;
+using TABP.Domain.Interfaces.Services.Pdf;
+namespace TABP.Infrastructure.Services.Pdf;
+
+public class PdfService : IPdfService
+{
+    public Task<byte[]> GeneratePdfAsync(string html)
+    {
+        return Task.Run(() =>
+        {
+            var htmlToPdfConverter = new HtmlToPdfConverter();
+            return htmlToPdfConverter.GeneratePdf(html);
+        });
+    }
+}
