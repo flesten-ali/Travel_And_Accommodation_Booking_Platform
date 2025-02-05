b1119a9a78b321a6386bcbbd98b6a72126e08154
flesten-ali <fbada858@gmail.com>
2025-02-05 14:45:46 +0200

    Add Room repository

---
+++ TABP.Infrastructure/Persistence/Repositories/RoomRepository.cs
@@ -0,0 +1,9 @@
+﻿using TABP.Domain.Entities;
+using TABP.Domain.Interfaces.Persistence.Repositories;
+using TABP.Infrastructure.Persistence.DbContexts;
+
+namespace TABP.Infrastructure.Persistence.Repositories;
+
+public class RoomRepository(AppDbContext context) : Repository<Room>(context), IRoomRepository
+{
+}
