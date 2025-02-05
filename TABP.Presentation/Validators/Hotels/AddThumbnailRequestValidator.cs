ee25d2005286eea9bac2f171dd0475861c5a0587
flesten-ali <fbada858@gmail.com>
2025-02-05 14:03:35 +0200

    Add Thumbnail Request Validator

---
+++ TABP.Presentation/Validators/Hotels/AddThumbnailRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using TABP.Presentation.DTOs;
+
+namespace TABP.Presentation.Validators.Hotels;
+public class AddThumbnailRequestValidator : AbstractValidator<AddThumbnailRequest>
+{
+    public AddThumbnailRequestValidator()
+    {
+        RuleFor(x => x.Thumbnail).NotEmpty().WithMessage("Thumbnail image is required");
+        RuleFor(x => x.HotelId).NotEmpty().WithMessage("Hotel id is required");
+    }
+}
