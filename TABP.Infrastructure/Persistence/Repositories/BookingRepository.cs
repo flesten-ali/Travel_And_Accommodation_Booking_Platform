367ec5cae1253292e3257bdcb81df07bfa6bcfdf
flesten-ali <fbada858@gmail.com>
2025-02-05 23:14:06 +0200

    Add Booking repository

---
+++ TABP.Infrastructure/Persistence/Repositories/BookingRepository.cs
@@ -0,0 +1,8 @@
+﻿using TABP.Domain.Entities;
+using TABP.Domain.Interfaces.Persistence.Repositories;
+using TABP.Infrastructure.Persistence.DbContexts;
+
+namespace TABP.Infrastructure.Persistence.Repositories;
+public class BookingRepository(AppDbContext context) : Repository<Booking>(context), IBookingRepository
+{
+}
