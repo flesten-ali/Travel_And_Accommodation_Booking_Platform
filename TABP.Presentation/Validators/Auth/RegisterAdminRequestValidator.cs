ef2bd93817c921fac7edcc9dea0c0f93d8df9195
flesten-ali <fbada858@gmail.com>
2025-02-05 14:01:31 +0200

    Add fluent validation for register admin request

---
+++ TABP.Presentation/Validators/Auth/RegisterAdminRequestValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using TABP.Presentation.DTOs.Auth;
+
+namespace TABP.Presentation.Validators.Auth;
+public class RegisterAdminRequestValidator : AbstractValidator<RegisterUserRequest>
+{
+    public RegisterAdminRequestValidator()
+    {
+        RuleFor(u => u.Email)
+            .NotEmpty()
+            .EmailAddress()
+            .WithMessage("Email address is required!");
+
+        RuleFor(u => u.UserName)
+            .NotEmpty()
+            .MinimumLength(3);
+
+        RuleFor(x => x.Password)
+                    .NotEmpty()
+                    .WithMessage("password is required!")
+                    .MinimumLength(8).WithMessage("Password must be at least 8 characters long.")
+                    .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+                    .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+                    .Matches(@"\d").WithMessage("Password must contain at least one digit.")
+                    .Matches(@"[\@\!\?\*\$]").WithMessage("Password must contain at least one special character (@, !, ?, *, $).");
+    }
+}
