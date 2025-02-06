028c56e99bdc7f0598a2e12408497c42d047e5b0
flesten-ali <fbada858@gmail.com>
2025-02-06 14:17:07 +0200

    Add Booking controller

---
+++ TABP.Presentation/Controllers/BookingController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TABP.Application.Bookings.Add;
+using TABP.Presentation.DTOs.Booking;
+
+namespace TABP.Presentation.Controllers;
+[Route("api/bookings")]
+[ApiController]
+public class BookingController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly IMapper _mapper;
+
+    public BookingController(IMediator mediator, IMapper mapper)
+    {
+        _mediator = mediator;
+        _mapper = mapper;
+    }
+
+    [HttpPost("book")]
+    public async Task<IActionResult> Add([FromBody] AddBookingRequest request)
+    {
+        var command = _mapper.Map<AddBookingCommand>(request);
+        var result = await _mediator.Send(command);
+        return Ok(result);
+    }
+}
