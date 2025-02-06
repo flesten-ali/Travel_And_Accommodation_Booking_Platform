c12b06fb33196d828fbdba3c03455b1690924ee0
flesten-ali <fbada858@gmail.com>
2025-02-06 14:17:23 +0200

    Add Booking request dto

---
+++ TABP.Presentation/DTOs/Booking/AddBookingRequest.cs
@@ -0,0 +1,14 @@
+﻿using TABP.Domain.Enums;
+
+namespace TABP.Presentation.DTOs.Booking;
+public class AddBookingRequest
+{
+    public PaymentMethod PaymentMethod { get; set; }
+    public DateTime CheckInDate { get; set; }
+    public DateTime CheckOutDate { get; set; }
+    public DateTime BookingDate { get; set; }
+    public string? Remarks { get; set; }
+    public decimal TotalPrice { get; set; }
+    public Guid UserId { get; set; }
+    public IEnumerable<Guid> RoomIds { get; set; } = [];
+}
