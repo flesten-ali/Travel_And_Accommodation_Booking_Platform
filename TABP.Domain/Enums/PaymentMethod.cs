e5f89e8a4140ca6285bf0c2443ff10bfc731276f
flesten-ali <fbada858@gmail.com>
2025-02-06 14:16:39 +0200

    Update payment method to appear string in JSON

--- TABP.Domain/Enums/PaymentMethod.cs
+++ TABP.Domain/Enums/PaymentMethod.cs
@@ -1,5 +1,7 @@
-﻿namespace TABP.Domain.Enums;
+﻿using System.Text.Json.Serialization;
+namespace TABP.Domain.Enums;
 
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum PaymentMethod
 {
     Visa,
