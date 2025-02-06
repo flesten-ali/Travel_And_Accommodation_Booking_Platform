895027a1b1725fff42ffa6482cdcd38186146bae
flesten-ali <fbada858@gmail.com>
2025-02-06 14:17:38 +0200

    Add Booking profile

---
+++ TABP.Presentation/Profiles/BookingProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using TABP.Application.Bookings.Add;
+using TABP.Presentation.DTOs.Booking;
+
+namespace TABP.Presentation.Profiles;
+public class BookingProfile : Profile
+{
+    public BookingProfile()
+    {
+        CreateMap<AddBookingRequest, AddBookingCommand>();
+    }
+}
