c1c893a94e28e713c09160b45905c60597685b0c
flesten-ali <fbada858@gmail.com>
2025-02-07 23:30:36 +0200

    Add GetByHotelIdAsync

--- TABP.Domain/Interfaces/Persistence/Repositories/IRoomClassRepository.cs
+++ TABP.Domain/Interfaces/Persistence/Repositories/IRoomClassRepository.cs
@@ -1,6 +1,8 @@
 ﻿using TABP.Domain.Entities;
+using TABP.Domain.Models;
 
 namespace TABP.Domain.Interfaces.Persistence.Repositories;
 public interface IRoomClassRepository : IRepository<RoomClass>
 {
+    Task<PaginatedList<RoomClass>> GetByHotelIdAsync(Guid hotelId, int pageSize, int pageNumber);
 }
