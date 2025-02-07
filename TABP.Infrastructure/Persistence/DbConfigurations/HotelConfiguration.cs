90a218598c6be450025becdc42e29f09453a52c8
flesten-ali <fbada858@gmail.com>
2025-02-07 20:11:57 +0200

    Update database remove un needed FKs

---
+++ TABP.Infrastructure/Persistence/DbConfigurations/HotelConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TABP.Domain.Entities;
+
+namespace TABP.Infrastructure.Persistence.DbConfigurations;
+public class HotelConfiguration : IEntityTypeConfiguration<Hotel>
+{
+    public void Configure(EntityTypeBuilder<Hotel> builder)
+    {
+        builder.Ignore(h => h.Gallery);
+    }
+}
