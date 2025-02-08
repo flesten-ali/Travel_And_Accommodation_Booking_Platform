5daf118f3e090ddf8098cfcd1597da42d65752c4
flesten-ali <fbada858@gmail.com>
2025-02-08 18:24:02 +0200

    Create AddToCart command handler

---
+++ TABP.Application/CartItems/AddToCart/AddToCartCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace TABP.Application.CartItems.AddToCart;
+public class AddToCartCommand : IRequest<Guid>
+{
+    public Guid UserId { get; set; }
+    public Guid RoomClassId { get; set; }
+}
