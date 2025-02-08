2e16da5be7c3be713c85b8d9495e23fe296c0ee0
flesten-ali <fbada858@gmail.com>
2025-02-08 13:52:48 +0200

    Validate GetRoomClassDetailsRequest

---
+++ TABP.Presentation/Validators/RoomClass/GetRoomClassDetailsRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using TABP.Presentation.DTOs.RoomClass;
+
+namespace TABP.Presentation.Validators.RoomClass;
+public class GetRoomClassDetailsRequestValidator : AbstractValidator<GetRoomClassDetailsRequest>
+{
+    public GetRoomClassDetailsRequestValidator()
+    {
+        RuleFor(r => r.HotelId)
+            .NotEmpty().WithMessage("HotelId is required.");
+
+        RuleFor(r => r.PageSize)
+            .InclusiveBetween(1, 100).WithMessage("Page size must be between 1 and 100.");
+
+        RuleFor(r => r.PageNumber)
+            .GreaterThan(0).WithMessage("Page number must be greater than 0.");
+    }
+}
