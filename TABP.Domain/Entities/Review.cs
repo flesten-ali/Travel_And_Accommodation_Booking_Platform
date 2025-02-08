bd79878a5029d1282f855c1989dec6d38f74e2c7
flesten-ali <fbada858@gmail.com>
2025-02-08 16:16:45 +0200

    Add CreatedAt col

--- TABP.Domain/Entities/Review.cs
+++ TABP.Domain/Entities/Review.cs
@@ -4,6 +4,7 @@ public class Review : EntityBase<Guid>
 {
     public string Comment { get; set; }
     public int Rate { get; set; }
+    public DateTime CreatedAt { get; set; }
     public Hotel Hotel { get; set; }
     public Guid HotelId { get; set; }
     public User User { get; set; }
