6240bd18e7273569e8ccb25ae58747261a858a09
flesten-ali <fbada858@gmail.com>
2025-02-08 16:19:11 +0200

    Add review profile

---
+++ TABP.Application/Profiles/ReviewProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using TABP.Application.Reviews.Queries.GetDetails;
+using TABP.Domain.Entities;
+using TABP.Domain.Models;
+
+namespace TABP.Application.Profiles;
+public class ReviewProfile : Profile
+{
+    public ReviewProfile()
+    {
+        CreateMap<Review, GetReviewDetailsQueryReponse>()
+            .ForMember(dest => dest.ReviwerName, opt => opt.MapFrom(src => src.User.UserName));
+
+        CreateMap<PaginatedList<Review>, PaginatedList<GetReviewDetailsQueryReponse>>();
+    }
+}
