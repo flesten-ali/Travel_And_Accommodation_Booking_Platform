ea77a6d5e12781c0e10a5b837aa6425224161b44
flesten-ali <fbada858@gmail.com>
2025-02-08 19:17:54 +0200

    Update namespace

---
+++ TABP.Application/Bookings/Commands/Add/AddBookingCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using TABP.Domain.Enums;
+namespace TABP.Application.Bookings.Commands.Add;
+
+public class AddBookingCommand : IRequest<Guid>
+{
+    public PaymentMethod PaymentMethod { get; set; }
+    public DateTime CheckInDate { get; set; }
+    public DateTime CheckOutDate { get; set; }
+    public DateTime BookingDate { get; set; }
+    public string? Remarks { get; set; }
+    public decimal TotalPrice { get; set; }
+    public Guid UserId { get; set; }
+    public IEnumerable<Guid> RoomIds { get; set; } = [];
+}
