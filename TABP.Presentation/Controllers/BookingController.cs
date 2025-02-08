d93ba5ee06d7ce0b3fb32722c379cc53766f09a2
flesten-ali <fbada858@gmail.com>
2025-02-08 19:52:22 +0200

    Refactor Booking controller

--- TABP.Presentation/Controllers/BookingController.cs
+++ TABP.Presentation/Controllers/BookingController.cs
@@ -1,13 +1,17 @@
 ﻿using AutoMapper;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using TABP.Application.Bookings.Commands.Add;
+using TABP.Application.Bookings.Commands.Create;
 using TABP.Application.Bookings.Queries.PdfConfirmation;
+using TABP.Domain.Constants;
 using TABP.Presentation.DTOs.Booking;
-
 namespace TABP.Presentation.Controllers;
-[Route("api/[controller]")]
+
+[Route("api/bookings")]
 [ApiController]
+[Authorize(Roles = Roles.Guest)]
 public class BookingController : ControllerBase
 {
     private readonly IMediator _mediator;
@@ -19,19 +23,33 @@ public class BookingController : ControllerBase
         _mapper = mapper;
     }
 
-    [HttpPost("book")]
-    public async Task<IActionResult> Add([FromBody] AddBookingRequest request)
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+
+    public async Task<IActionResult> CreateBooking([FromBody] AddBookingRequest request)
     {
-        var command = _mapper.Map<AddBookingCommand>(request);
+        var command = _mapper.Map<CreateBookingCommand>(request);
+
         var result = await _mediator.Send(command);
+
         return Ok(result);
     }
 
-    [HttpPost("{id:guid}/get-pdf-cofirmation")]
-    public async Task<IActionResult> GetPdfConfirmation(Guid id)
+    [HttpPost("{id:guid}/invoice")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public async Task<IActionResult> GetInvoicePdf(Guid id)
     {
-        var query = new GetPdfConfirmationQuery { BookingId = id };
+        var query = new GetInvoicePdfQuery { BookingId = id };
+
         var result = await _mediator.Send(query);
-        return Ok(result);
+
+        return File(result.PdfContent, "application/pdf", "invoice.pdf");
     }
 }
