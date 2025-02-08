fbbaf2027fe0815d29c0e9d91889432b1bec664c
flesten-ali <fbada858@gmail.com>
2025-02-08 19:18:14 +0200

    Create GetPdfConfirmation query

---
+++ TABP.Application/Bookings/Queries/PdfConfirmation/GetPdfConfirmationQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+
+namespace TABP.Application.Bookings.Queries.PdfConfirmation;
+public class GetPdfConfirmationQuery : IRequest<GetPdfConfirmationResponse>
+{
+    public Guid BookingId { get; set; }
+}
