43724fe290eb84a04fa4663975a46e2f4f7c470b
flesten-ali <fbada858@gmail.com>
2025-02-08 17:18:33 +0200

    Remove un needed cols

--- TABP.Domain/Entities/CartItem.cs
+++ TABP.Domain/Entities/CartItem.cs
@@ -2,9 +2,6 @@
 
 public class CartItem : EntityBase<Guid>
 {
-    public DateTime CheckInDate { get; set; }
-    public DateTime CheckOutDate { get; set; }
-    public string? Remarks { get; set; }
     public User User { get; set; }
     public Guid UserId { get; set; }
     public RoomClass RoomClass { get; set; }
