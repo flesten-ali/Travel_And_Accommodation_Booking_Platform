1d972bb375a27bce8cc8f3648218b5a1f75c87f0
flesten-ali <fbada858@gmail.com>
2025-02-08 16:18:56 +0200

    Add review repo

---
+++ TABP.Domain/Interfaces/Persistence/Repositories/IReviewRepository.cs
@@ -0,0 +1,8 @@
+﻿using TABP.Domain.Entities;
+using TABP.Domain.Models;
+namespace TABP.Domain.Interfaces.Persistence.Repositories;
+
+public interface IReviewRepository : IRepository<Review>
+{
+    Task<PaginatedList<Review>?> GetByHotelIdAsync(Guid hotelId, int pageSize, int pageNumber);
+}
---
+++ TABP.Infrastructure/Persistence/Repositories/ReviewRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TABP.Domain.Entities;
+using TABP.Domain.Interfaces.Persistence.Repositories;
+using TABP.Domain.Models;
+using TABP.Infrastructure.Extenstions;
+using TABP.Infrastructure.Persistence.DbContexts;
+namespace TABP.Infrastructure.Persistence.Repositories;
+
+public class ReviewRepository(AppDbContext context) : Repository<Review>(context), IReviewRepository
+{
+    public async Task<PaginatedList<Review>?> GetByHotelIdAsync(Guid hotelId, int pageSize, int pageNumber)
+    {
+        var reviews = DbSet.Where(r => r.HotelId == hotelId).Include(r=>r.User);
+
+        if (reviews == null)
+        {
+            return null;
+        }
+
+        var requstedPage = reviews.GetRequestedPage(pageNumber, pageSize);
+        var paginationMetaData = await requstedPage.GetPaginationMetaDataAsync(pageNumber, pageSize);
+
+        return new PaginatedList<Review>(await requstedPage.ToListAsync(), paginationMetaData);
+    }
+}
