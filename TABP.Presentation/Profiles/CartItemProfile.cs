e7dcbf3dbe570250a71c743cede7ee505402ab6b
flesten-ali <fbada858@gmail.com>
2025-02-08 18:25:58 +0200

    Create AddToCart Profile

---
+++ TABP.Presentation/Profiles/CartItemProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using TABP.Application.CartItems.AddToCart;
+using TABP.Presentation.DTOs.CartItem;
+
+namespace TABP.Presentation.Profiles;
+public class CartItemProfile :Profile
+{
+    public CartItemProfile()
+    {
+        CreateMap<AddToCartRequest, AddToCartCommand>();
+    }
+}
