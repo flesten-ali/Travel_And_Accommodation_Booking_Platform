140be176aeba2532822fcdabd14244ad2379c6bc
flesten-ali <fbada858@gmail.com>
2025-02-08 18:26:14 +0200

    Create AddToCartRequest Validator

---
+++ TABP.Presentation/Validators/CartItem/AddToCartRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using TABP.Presentation.DTOs.CartItem;
+
+namespace TABP.Presentation.Validators.CartItem;
+public class AddToCartRequestValidator : AbstractValidator<AddToCartRequest>
+{
+    public AddToCartRequestValidator()
+    {
+        RuleFor(c => c.UserId)
+            .NotEmpty().WithMessage("UserId is required.");
+
+        RuleFor(c => c.RoomClassId)
+         .NotEmpty().WithMessage("RoomClassId is required.");
+    }
+}
