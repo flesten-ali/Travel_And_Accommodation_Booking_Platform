6b5ef01d612879915789ef925d39b61ce03a0cc2
flesten-ali <fbada858@gmail.com>
2025-02-09 17:31:32 +0200

    Update name

--- TABP.Application/Profiles/RoomClassesProfile.cs
+++ TABP.Application/Profiles/RoomClassesProfile.cs
@@ -8,7 +8,7 @@ public class RoomClassesProfile : Profile
 {
     public RoomClassesProfile()
     {
-        CreateMap<RoomClass, GetHotelRoomClassesQueryResponse>()
+        CreateMap<RoomClass, HotelRoomClassesQueryResponse>()
             .ForMember(dest => dest.GalleryUrls, opt => opt.MapFrom(src => src.Gallery.Select(g => g.ImageUrl)))
             .ForMember(dest => dest.DiscountResponses, opt => opt.MapFrom(src => src.Discounts))
             .ForMember(dest => dest.AmenityResponses, opt => opt.MapFrom(src => src.Amenities));
@@ -17,6 +17,6 @@ public class RoomClassesProfile : Profile
 
         CreateMap<Amenity, AmenityResponse>();
 
-        CreateMap<PaginatedList<RoomClass>, PaginatedList<GetHotelRoomClassesQueryResponse>>();
+        CreateMap<PaginatedList<RoomClass>, PaginatedList<HotelRoomClassesQueryResponse>>();
     }
 }
