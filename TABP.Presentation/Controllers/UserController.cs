961add27fa2b30df8454580eadcc0445ef8c9a39
flesten-ali <fbada858@gmail.com>
2025-02-09 15:59:36 +0200

    Update user controller

--- TABP.Presentation/Controllers/UserController.cs
+++ TABP.Presentation/Controllers/UserController.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TABP.Application.Users.Login;
 using TABP.Application.Users.Register;
@@ -7,7 +9,7 @@ using TABP.Domain.Constants;
 using TABP.Presentation.DTOs.Auth;
 namespace TABP.Presentation.Controllers;
 
-[Route("api/[controller]")]
+[Route("api/users")]
 [ApiController]
 public class UserController : ControllerBase
 {
@@ -20,20 +22,47 @@ public class UserController : ControllerBase
         _mapper = mapper;
     }
 
-    [HttpPost("register")]
-    public async Task<IActionResult> Register([FromBody] RegisterUserRequest request)
-    {
-        var command = _mapper.Map<RegisterUserCommand>(request);
-        command.Role = Roles.Guest; 
-        var result = await _mediator.Send(command);
-        return Ok(result);
-    }
-
     [HttpPost("login")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     public async Task<IActionResult> Login([FromBody] LoginUserRequest request)
     {
         var command = _mapper.Map<LoginUserCommand>(request);
+
         var result = await _mediator.Send(command);
+
         return Ok(result);
+    }
+
+    [HttpPost("register-user")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    public async Task<IActionResult> RegisterUser([FromBody] RegisterUserRequest request)
+    {
+        var command = _mapper.Map<RegisterUserCommand>(request);
+        command.Role = Roles.Guest;
+
+        await _mediator.Send(command);
+
+        return Created();
+    }
+
+    [HttpPost("register-admin")]
+    [Authorize(Roles = Roles.Admin)]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status409Conflict)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    public async Task<IActionResult> RegisterAdmin([FromBody] RegisterAdminRequest request)
+    {
+        var command = _mapper.Map<RegisterUserCommand>(request);
+        command.Role = Roles.Admin;
+
+        await _mediator.Send(command);
+
+        return Created();
     }
 }
