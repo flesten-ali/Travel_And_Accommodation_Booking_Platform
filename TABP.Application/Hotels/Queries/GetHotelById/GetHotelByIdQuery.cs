dd5dbb176941088cb4c8dfd02e3ddee38850889f
flesten-ali <fbada858@gmail.com>
2025-02-09 14:13:07 +0200

    Create GetHotelById

---
+++ TABP.Application/Hotels/Queries/GetHotelById/GetHotelByIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using TABP.Application.Hotels.Common;
+
+namespace TABP.Application.Hotels.Queries.GetHotelById;
+public class GetHotelByIdQuery : IRequest<HotelResponse>
+{
+    public Guid HotelId { get; set; }
+}
---
+++ TABP.Application/Hotels/Queries/GetHotelById/GetHotelByIdQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using TABP.Application.Hotels.Common;
+using TABP.Domain.Exceptions;
+using TABP.Domain.Interfaces.Persistence.Repositories;
+
+namespace TABP.Application.Hotels.Queries.GetHotelById;
+public class GetHotelByIdQueryHandler : IRequestHandler<GetHotelByIdQuery, HotelResponse>
+{
+    private readonly IHotelRepository _hotelRepository;
+    private readonly IMapper _mapper;
+
+    public GetHotelByIdQueryHandler(IHotelRepository hotelRepository, IMapper mapper)
+    {
+        _hotelRepository = hotelRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<HotelResponse> Handle(GetHotelByIdQuery request, CancellationToken cancellationToken)
+    {
+        var hotel = await _hotelRepository.GetByIdIncludeProperties(request.HotelId, h => h.Owner, h => h.City)
+            ?? throw new NotFoundException("Hotel not found");
+
+        return _mapper.Map<HotelResponse>(hotel);
+    }
+}
