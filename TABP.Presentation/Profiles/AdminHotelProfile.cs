ced93b051ceca2fd19f08de43f5e4184c792e6f5
flesten-ali <fbada858@gmail.com>
2025-02-09 14:38:36 +0200

    Update namespace

--- TABP.Presentation/Profiles/AdminHotelProfile.cs
+++ TABP.Presentation/Profiles/AdminHotelProfile.cs
@@ -1,7 +1,7 @@
 ﻿using AutoMapper;
 using TABP.Application.Hotels.Commands.AddHotel;
-using TABP.Application.Hotels.Commands.AddImageGallery;
-using TABP.Application.Hotels.Commands.AddThumbnail;
+using TABP.Application.Hotels.Commands.ImageGallery;
+using TABP.Application.Hotels.Commands.Thumbnail;
 using TABP.Presentation.DTOs;
 using TABP.Presentation.DTOs.Hotel;
 namespace TABP.Presentation.Profiles;
@@ -10,8 +10,8 @@ public class AdminHotelProfile : Profile
 {
     public AdminHotelProfile()
     {
-        CreateMap<AddHotelRequest, AddHotelCommand>();
-        CreateMap<AddThumbnailRequest, AddThumbnailCommand>();
-        CreateMap<AddImageGalleryRequest, AddImageGalleryCommand>();
+        CreateMap<CreateHotelRequest, CreateHotelCommand>();
+        CreateMap<UploadThumbnailRequest, UploadThumbnailCommand>();
+        CreateMap<UploadImageGalleryRequest, UploadImageGalleryCommand>();
     }
 }
