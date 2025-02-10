d71f3b10c5bf4c603fb5ed93e6b9c8a2749f3ea3
flesten-ali <fbada858@gmail.com>
2025-02-10 20:33:18 +0200

    Update getting thumbnail image in SearchHotels

--- TABP.Infrastructure/Persistence/Repositories/HotelRepository.cs
+++ TABP.Infrastructure/Persistence/Repositories/HotelRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 using TABP.Domain.Entities;
+using TABP.Domain.Enums;
 using TABP.Domain.Interfaces.Persistence.Repositories;
 using TABP.Domain.Models;
 using TABP.Infrastructure.Extenstions;
@@ -24,7 +25,7 @@ public class HotelRepository(AppDbContext context) : Repository<Hotel>(context),
             Description = h.Description,
             StarRating = h.Rate,
             PricePerNight = h.RoomClasses.Min(rc => rc.Price),
-            Thumbnail = context.Images.Where(img => img.Id == h.ThumbnailId && img.ImageableId == h.Id).FirstOrDefault()
+            Thumbnail = context.Images.Where(img => img.ImageableId == h.Id && img.ImageType == ImageType.Thumbnail).FirstOrDefault()
         });
 
         var resultToReturn = selectedResult.GetRequestedPage(pageNumber, pageSize);
