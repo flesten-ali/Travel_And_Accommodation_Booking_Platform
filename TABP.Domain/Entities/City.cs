973a44515aa706b007875df2da802126ce021c53
flesten-ali <fbada858@gmail.com>
2025-02-10 20:05:29 +0200

    Add thumbnail column to the city entity

--- TABP.Domain/Entities/City.cs
+++ TABP.Domain/Entities/City.cs
@@ -6,4 +6,6 @@ public class City : EntityBase<Guid>
     public string PostalCode { get; set; }
     public string Address { get; set; }
     public ICollection<Hotel> Hotels { get; set; } = [];
+    public Image Thumbnail { get; set; }
+    public  Guid ThumbnailId { get; set; } 
 }
