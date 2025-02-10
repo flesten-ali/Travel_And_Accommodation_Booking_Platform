7f4e357bd43567508c2b97ccb675d39e6f2b8c23
flesten-ali <fbada858@gmail.com>
2025-02-10 23:41:03 +0200

    Add GetRecentlyVisitedHotelsRequest fluent validations

---
+++ TABP.Presentation/Validators/Guest/GetRecentlyVisitedHotelsRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using TABP.Presentation.DTOs.Guest;
+namespace TABP.Presentation.Validators.Guest;
+
+public class GetRecentlyVisitedHotelsRequestValidator : AbstractValidator<GetRecentlyVisitedHotelsRequest>
+{
+    public GetRecentlyVisitedHotelsRequestValidator()
+    {
+        RuleFor(x => x.Limit)
+               .GreaterThan(0).WithMessage("Limit must be greater than 0.");
+    }
+}
