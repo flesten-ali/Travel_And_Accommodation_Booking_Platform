f5ce94c2e34f9bedc8871a9a98e6319a9f1b1dea
flesten-ali <fbada858@gmail.com>
2025-02-10 18:39:55 +0200

    Create GetRecentlyVisitedHotelsQueryHandler

---
+++ TABP.Application/Hotels/Queries/GetRecentlyVisited/GetRecentlyVisitedHotelsQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using MediatR;
+using TABP.Domain.Exceptions;
+using TABP.Domain.Interfaces.Persistence.Repositories;
+
+namespace TABP.Application.Hotels.Queries.GetRecentlyVisited;
+public class GetRecentlyVisitedHotelsQueryHandler
+    : IRequestHandler<GetRecentlyVisitedHotelsQuery, IEnumerable<RecentlyVisitedHotelsResponse>>
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IBookingRepository _bookingRepository;
+    private readonly IMapper _mapper;
+
+    public GetRecentlyVisitedHotelsQueryHandler(
+        IUserRepository userRepository,
+         IBookingRepository bookingRepository,
+        IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _bookingRepository = bookingRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<RecentlyVisitedHotelsResponse>> Handle(
+        GetRecentlyVisitedHotelsQuery request,
+        CancellationToken cancellationToken)
+    {
+        if (!await _userRepository.ExistsAsync(u => u.Id == request.GuestId))
+        {
+            throw new NotFoundException("User not found");
+        }
+
+        var recentlyVisitedHotels = await _bookingRepository.GetRecentlyVisitedHotels(request.GuestId, request.Limit);
+
+        return _mapper.Map<IEnumerable<RecentlyVisitedHotelsResponse>>(recentlyVisitedHotels);
+    }
+}
