31565d1b5dc5454556c733e18b7f4ac18ad94f95
flesten-ali <fbada858@gmail.com>
2025-02-12 23:02:08 +0200

    Create Common folder for the city response

---
+++ TABP.Application/Cities/Common/CityResponse.cs
@@ -0,0 +1,12 @@
+﻿using TABP.Domain.Entities;
+
+namespace TABP.Application.Cities.Common;
+public class CityResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Country { get; set; }
+    public string PostOffice { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+}
