0959f1a9e05aa67cfc19ed39d72ae88374cd60ac
flesten-ali <fbada858@gmail.com>
2025-02-12 23:01:50 +0200

    Add Create City functionality

---
+++ TABP.Application/Cities/Commands/Create/CreateCityCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using TABP.Application.Cities.Common;
+namespace TABP.Application.Cities.Commands.Create;
+
+public class CreateCityCommand : IRequest<CityResponse>
+{
+    public string Name { get; set; }
+    public string? PostalCode { get; set; }
+    public string? Address { get; set; }
+    public string Country { get; set; }
+    public string PostOffice { get; set; }
+}
---
+++ TABP.Application/Cities/Commands/Create/CreateCityCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using MediatR;
+using TABP.Application.Cities.Common;
+using TABP.Domain.Entities;
+using TABP.Domain.Interfaces.Persistence;
+using TABP.Domain.Interfaces.Persistence.Repositories;
+
+namespace TABP.Application.Cities.Commands.Create;
+public class CreateCityCommandHandler : IRequestHandler<CreateCityCommand, CityResponse>
+{
+    private readonly ICityRepository _cityRepository;
+    private readonly IMapper _mapper;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateCityCommandHandler(ICityRepository cityRepository, IMapper mapper, IUnitOfWork unitOfWork)
+    {
+        _cityRepository = cityRepository;
+        _mapper = mapper;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<CityResponse> Handle(CreateCityCommand request, CancellationToken cancellationToken)
+    {
+        var city = _mapper.Map<City>(request);
+
+        await _cityRepository.AddAsync(city);
+        await _unitOfWork.SaveChangesAsync();
+
+        return _mapper.Map<CityResponse>(city);
+    }
+}
