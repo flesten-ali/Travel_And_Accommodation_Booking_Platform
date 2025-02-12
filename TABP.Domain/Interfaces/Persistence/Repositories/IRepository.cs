30f31639cbd12f46849f77b0cd501bbe3940cbd3
flesten-ali <fbada858@gmail.com>
2025-02-12 16:56:55 +0200

    Remove extra spaces

--- TABP.Domain/Interfaces/Persistence/Repositories/IRepository.cs
+++ TABP.Domain/Interfaces/Persistence/Repositories/IRepository.cs
@@ -8,6 +8,5 @@ public interface IRepository<T> where T : class
     Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate);
     Task<IEnumerable<T>> GetAllByIdAsync(IEnumerable<Guid> Ids);
     Task<T?> GetByIdAsync(Guid id);
-
     Task<T?> GetByIdIncludeProperties(Guid entityId, params Expression<Func<T, object>>[] includeProperties);
 }
