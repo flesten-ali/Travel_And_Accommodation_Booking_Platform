2f3bd61235ed9e09c5e5abeee43e34f885f93e8a
flesten-ali <fbada858@gmail.com>
2025-02-12 23:03:31 +0200

    Add CreateCity action and GetCity

--- TABP.Presentation/Controllers/CitiesController.cs
+++ TABP.Presentation/Controllers/CitiesController.cs
@@ -4,19 +4,24 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Text.Json;
+using TABP.Application.Cities.Commands.Create;
+using TABP.Application.Cities.Queries.GetById;
 using TABP.Application.Cities.Queries.GetForAdmin;
+using TABP.Application.Cities.Queries.GetTrending;
 using TABP.Domain.Constants;
 using TABP.Presentation.DTOs.City;
 namespace TABP.Presentation.Controllers;
 
 [Route("api/cities")]
 [ApiController]
+//[Authorize(Roles = Roles.Admin)]
 public class CitiesController(IMediator mediator, IMapper mapper) : ControllerBase
 {
     private readonly IMediator _mediator = mediator;
     private readonly IMapper _mapper = mapper;
 
     [HttpGet("trending-cities")]
+    [AllowAnonymous]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> GetTrendingCities(int limit)
@@ -29,7 +34,6 @@ public class CitiesController(IMediator mediator, IMapper mapper) : ControllerBa
     }
 
     [HttpGet("cities-for-admin")]
-    [Authorize(Roles = Roles.Admin)]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status403Forbidden)]
@@ -44,5 +48,34 @@ public class CitiesController(IMediator mediator, IMapper mapper) : ControllerBa
             JsonSerializer.Serialize(cities.PaginationMetaData));
 
         return Ok(cities.Items);
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> CreateCity([FromBody] CreateCityRequest request)
+    {
+        var command = _mapper.Map<CreateCityCommand>(request);
+
+        var city = await _mediator.Send(command);
+
+        return CreatedAtAction(nameof(GetCity), new { id = city.Id }, city);
+    }
+
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> GetCity(Guid id)
+    {
+        var query = new GetCityByIdQuery { Id = id };
+
+        var city = await _mediator.Send(query);
+
+        return Ok(city);
     }
 }
