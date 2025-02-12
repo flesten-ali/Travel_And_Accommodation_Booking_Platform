f99c0b6f40367eb2bd1abb583d8df822996126c5
flesten-ali <fbada858@gmail.com>
2025-02-12 19:08:09 +0200

    Create GetCitiesForAdminQuery

---
+++ TABP.Application/Cities/GetForAdmin/GetCitiesForAdminQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using TABP.Domain.Models;
+
+namespace TABP.Application.Cities.GetForAdmin;
+public class GetCitiesForAdminQuery : IRequest<PaginatedList<CityForAdminResponse>>
+{
+    public int PageSize { get; set; }
+    public int PageNumber { get; set; }
+}
