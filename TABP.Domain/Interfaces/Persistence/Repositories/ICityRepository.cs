f3918cf8fe7a3850935d847ca3c73db3263464b8
flesten-ali <fbada858@gmail.com>
2025-02-12 19:09:13 +0200

    Create and implement GetCitiesForAdmin functionality

--- TABP.Domain/Interfaces/Persistence/Repositories/ICityRepository.cs
+++ TABP.Domain/Interfaces/Persistence/Repositories/ICityRepository.cs
@@ -1,6 +1,8 @@
 ﻿using TABP.Domain.Entities;
+using TABP.Domain.Models;
 
 namespace TABP.Domain.Interfaces.Persistence.Repositories;
 public interface ICityRepository : IRepository<City>
 {
+    Task<PaginatedList<CityForAdminResult>> GetCitiesForAdmin(int pageSize, int pageNumber);
 }
--- TABP.Infrastructure/Persistence/Repositories/CityRepository.cs
+++ TABP.Infrastructure/Persistence/Repositories/CityRepository.cs
@@ -1,10 +1,27 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TABP.Domain.Entities;
 using TABP.Domain.Interfaces.Persistence.Repositories;
+using TABP.Domain.Models;
+using TABP.Infrastructure.Extenstions;
 using TABP.Infrastructure.Persistence.DbContexts;
 
 namespace TABP.Infrastructure.Persistence.Repositories;
 public class CityRepository(AppDbContext context) : Repository<City>(context), ICityRepository
 {
- 
+    public async Task<PaginatedList<CityForAdminResult>> GetCitiesForAdmin(int pageSize, int pageNumber)
+    {
+        var cities = DbSet.Select(c => new CityForAdminResult
+        {
+            Id = c.Id,
+            Name = c.Name,
+            Country = c.Country,
+            PostOffice = c.PostOffice,
+            CreatedDate = c.CreatedDate,
+            UpdatedDate = c.UpdatedDate,
+        });
+
+        var requestedPage = PaginationExtenstions.GetRequestedPage(cities, pageNumber, pageSize);
+        var paginationMetaData = await requestedPage.GetPaginationMetaDataAsync(pageNumber, pageSize);
+        return new PaginatedList<CityForAdminResult>(await requestedPage.ToListAsync(), paginationMetaData);
+    }
 }
