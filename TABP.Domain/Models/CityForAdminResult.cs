037b9c074f014363dad81d451974eff9b15775fb
flesten-ali <fbada858@gmail.com>
2025-02-12 19:08:32 +0200

    Create GetCitiesForAdmin response/result

---
+++ TABP.Domain/Models/CityForAdminResult.cs
@@ -0,0 +1,10 @@
+﻿namespace TABP.Domain.Models;
+public class CityForAdminResult
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Country { get; set; }
+    public string PostOffice { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+}
