2798d16356a734e57249e1222c96aa9e0ac300a0
flesten-ali <fbada858@gmail.com>
2025-02-13 16:06:02 +0200

    Update DeleteCityCommandHandler to delete city thumbnail

--- TABP.Application/Cities/Commands/Delete/DeleteCityCommandHandler.cs
+++ TABP.Application/Cities/Commands/Delete/DeleteCityCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using TABP.Domain.Enums;
 using TABP.Domain.Exceptions;
 using TABP.Domain.Interfaces.Persistence;
 using TABP.Domain.Interfaces.Persistence.Repositories;
@@ -8,20 +9,36 @@ public class DeleteCityCommandHandler : IRequestHandler<DeleteCityCommand>
 {
     private readonly ICityRepository _cityRepository;
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IImageRepository _imageRepository;
 
-    public DeleteCityCommandHandler(ICityRepository cityRepository, IUnitOfWork unitOfWork)
+    public DeleteCityCommandHandler(
+        ICityRepository cityRepository,
+        IUnitOfWork unitOfWork,
+        IImageRepository imageRepository)
     {
         _cityRepository = cityRepository;
         _unitOfWork = unitOfWork;
+        _imageRepository = imageRepository;
     }
     public async Task<Unit> Handle(DeleteCityCommand request, CancellationToken cancellationToken)
     {
         var city = await _cityRepository.GetByIdAsync(request.Id)
             ?? throw new NotFoundException("City not found");
 
-        _cityRepository.Delete(city);
-        await _unitOfWork.SaveChangesAsync();
+        await _unitOfWork.BeginTransactionAsync();
+        try
+        {
+            await _imageRepository.DeleteByIdAsync(request.Id, ImageType.Thumbnail);
+            _cityRepository.Delete(city);
 
-        return Unit.Value;
+            await _unitOfWork.CommitAsync();
+
+            return Unit.Value;
+        }
+        catch
+        {
+            await _unitOfWork.RollbackAsync();
+            throw;
+        }
     }
 }
