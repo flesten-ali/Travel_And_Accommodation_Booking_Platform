87b645670ccdebbcf46e4e8f8c7ab2f656d66bc3
flesten-ali <fbada858@gmail.com>
2025-02-13 15:05:31 +0200

    Move AdminHotelProfile content to HotelProfile

--- TABP.Presentation/Profiles/HotelProfile.cs
+++ TABP.Presentation/Profiles/HotelProfile.cs
@@ -1,5 +1,9 @@
 ﻿using AutoMapper;
+using TABP.Application.Hotels.Commands.Create;
+using TABP.Application.Hotels.Commands.ImageGallery;
+using TABP.Application.Hotels.Commands.Thumbnail;
 using TABP.Application.Hotels.Queries.SearchHotels;
+using TABP.Presentation.DTOs;
 using TABP.Presentation.DTOs.Hotel;
 namespace TABP.Presentation.Profiles;
 
@@ -7,6 +11,9 @@ public class HotelProfile : Profile
 {
     public HotelProfile()
     {
+        CreateMap<CreateHotelRequest, CreateHotelCommand>();
+        CreateMap<UploadHotelThumbnailRequest, UploadHotelThumbnailCommand>();
+        CreateMap<UploadImageGalleryRequest, UploadImageGalleryCommand>();
         CreateMap<SearchHotelRequest, SearchHotelsQuery>();
     }
 }
