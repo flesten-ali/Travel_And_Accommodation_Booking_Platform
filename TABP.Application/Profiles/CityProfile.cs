f48e60fdcff3b4ddb4dfe2a936ebc0375cf91a49
flesten-ali <fbada858@gmail.com>
2025-02-13 15:02:36 +0200

    Add Upload city thumbnail required mappings

--- TABP.Application/Profiles/CityProfile.cs
+++ TABP.Application/Profiles/CityProfile.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using TABP.Application.Cities.Commands.Create;
+using TABP.Application.Cities.Commands.Thumbnail;
 using TABP.Application.Cities.Common;
 using TABP.Application.Cities.Queries.GetTrending;
 using TABP.Domain.Entities;
+using TABP.Domain.Enums;
 using TABP.Domain.Models;
 
 namespace TABP.Application.Profiles;
@@ -11,9 +13,17 @@ public class CityProfile : Profile
     public CityProfile()
     {
         CreateMap<TrendingCitiesResult, TrendingCitiesResponse>();
+
         CreateMap<CityForAdminResult, CityResponse>();
+
         CreateMap<PaginatedList<CityForAdminResult>, PaginatedList<CityResponse>>();
+
         CreateMap<CreateCityCommand, City>();
+
         CreateMap<City, CityResponse>();
+
+        CreateMap<UploadCityThumbnailCommand, Image>()
+            .ForMember(dest => dest.ImageableId, opt => opt.MapFrom(src => src.CityId))
+            .ForMember(dest => dest.ImageType, opt => opt.MapFrom(src => ImageType.Thumbnail));
     }
 }
