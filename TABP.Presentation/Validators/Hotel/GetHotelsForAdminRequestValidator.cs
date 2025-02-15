af81ea85fc2c2a2d0fc408ea1309fe71741f89fd
flesten-ali <fbada858@gmail.com>
2025-02-15 13:42:38 +0200

    Create GetHotelsForAdminRequest validator

---
+++ TABP.Presentation/Validators/Hotel/GetHotelsForAdminRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using TABP.Presentation.DTOs.Hotel;
+
+namespace TABP.Presentation.Validators.Hotel;
+public class GetHotelsForAdminRequestValidator : AbstractValidator<GetHotelsForAdminRequest>
+{
+    public GetHotelsForAdminRequestValidator()
+    {
+        RuleFor(x => x.PageSize)
+            .InclusiveBetween(1, 100).WithMessage("Page size must be between 1 and 100.");
+
+        RuleFor(x => x.PageNumber)
+            .GreaterThan(0).WithMessage("Page number must be greater than 0.");
+    }
+}
