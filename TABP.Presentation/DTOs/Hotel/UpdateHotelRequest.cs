3c772afed2aef9190a0f525b4b322018502a28d4
flesten-ali <fbada858@gmail.com>
2025-02-15 16:07:39 +0200

    Added UpdateHotelRequest DTO for API requests

---
+++ TABP.Presentation/DTOs/Hotel/UpdateHotelRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace TABP.Presentation.DTOs.Hotel;
+public class UpdateHotelRequest
+{
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    public int Rate { get; set; }
+    public double LongitudeCoordinates { get; set; }
+    public double LatitudeCoordinates { get; set; }
+    public Guid CityId { get; set; }
+    public Guid OwnerId { get; set; }
+}
