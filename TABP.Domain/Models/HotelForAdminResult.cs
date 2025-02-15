a728ecd537fd177ff24eacc49911307fb92ba8b4
flesten-ali <fbada858@gmail.com>
2025-02-15 13:40:06 +0200

    Create HotelForAdminResult

---
+++ TABP.Domain/Models/HotelForAdminResult.cs
@@ -0,0 +1,12 @@
+﻿namespace TABP.Domain.Models;
+
+public class HotelForAdminResult
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public int Rate { get; set; }
+    public string OwnerName { get; set; }
+    public string CityName { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+}
