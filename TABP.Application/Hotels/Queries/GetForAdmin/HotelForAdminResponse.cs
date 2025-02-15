7336d7d76ed6c4ef3d2e01b749c2c7a54497a459
flesten-ali <fbada858@gmail.com>
2025-02-15 13:39:23 +0200

    Create HotelForAdminResponse

---
+++ TABP.Application/Hotels/Queries/GetForAdmin/HotelForAdminResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace TABP.Application.Hotels.Queries.GetForAdmin;
+public class HotelForAdminResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public int Rate { get; set; }
+    public string OwnerName { get; set; }
+    public string CityName { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+}
