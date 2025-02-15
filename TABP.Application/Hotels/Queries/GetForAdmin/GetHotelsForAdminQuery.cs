7a4e021c8634500b55c6f94b5b1442f14bcbf184
flesten-ali <fbada858@gmail.com>
2025-02-15 13:39:12 +0200

    Create GetHotelsForAdminQuery

---
+++ TABP.Application/Hotels/Queries/GetForAdmin/GetHotelsForAdminQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using TABP.Domain.Models;
+
+namespace TABP.Application.Hotels.Queries.GetForAdmin;
+public class GetHotelsForAdminQuery : IRequest<PaginatedList<HotelForAdminResponse>>
+{
+    public int PageSize { get; set; }
+    public int PageNumber { get; set; }
+}
