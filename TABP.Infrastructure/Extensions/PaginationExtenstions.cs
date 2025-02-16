b90a6d1a0e79b76ef0f8ecd039dd3c3b6895be5f
flesten-ali <fbada858@gmail.com>
2025-02-16 23:11:12 +0200

    Fix typo

--- TABP.Infrastructure/Extensions/PaginationExtenstions.cs
+++ TABP.Infrastructure/Extensions/PaginationExtenstions.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TABP.Domain.Models;
-namespace TABP.Infrastructure.Extenstions;
+namespace TABP.Infrastructure.Extensions;
 
 public static class PaginationExtenstions
 {
     public static IQueryable<T> GetRequestedPage<T>(this IQueryable<T> queryable, int pageSize, int pageNumber)
     {
-        return queryable.Skip((pageNumber - 1) * pageSize);
+        return queryable.Skip((pageNumber - 1) * pageSize).Take(pageSize);
     }
 
     public static async Task<PaginationMetaData> GetPaginationMetaDataAsync<T>(
