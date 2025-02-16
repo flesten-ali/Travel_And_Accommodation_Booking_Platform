0e03c7f9eff97c7dfb8de32f563deb5d06a1ed25
flesten-ali <fbada858@gmail.com>
2025-02-16 23:25:35 +0200

    Move PaginationParameters validations to be common

---
+++ TABP.Presentation/Validators/Common/PaginationParametersValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using TABP.Application;
+
+namespace TABP.Presentation.Validators.Common;
+public class PaginationParametersValidator : AbstractValidator<PaginationParameters>
+{
+    public PaginationParametersValidator()
+    {
+
+        RuleFor(x => x.PageSize)
+            .InclusiveBetween(1, 100).WithMessage("Page size must be between 1 and 100.");
+
+        RuleFor(x => x.PageNumber)
+            .GreaterThan(0).WithMessage("Page number must be greater than 0.");
+
+        RuleFor(x => x.OrderColumn)
+            .MaximumLength(20).WithMessage("SortBy cannot exceed 20 characters.")
+            .Must(value => string.IsNullOrEmpty(value) || value.All(char.IsLetter))
+            .WithMessage("SortBy must contain only lette");
+    }
+}
