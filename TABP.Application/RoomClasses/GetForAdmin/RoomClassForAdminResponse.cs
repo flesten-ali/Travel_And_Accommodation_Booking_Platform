badfce1acb99fc66ec6002e9c1e567f616d21685
flesten-ali <fbada858@gmail.com>
2025-02-16 14:11:17 +0200

    Added RoomClassForAdminResponse DTO

---
+++ TABP.Application/RoomClasses/GetForAdmin/RoomClassForAdminResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime;
+using TABP.Domain.Entities;
+using TABP.Domain.Enums;
+
+namespace TABP.Application.RoomClasses.GetForAdmin;
+public class RoomClassForAdminResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    public RoomType RoomType { get; set; }
+    public int NumberOfRooms { get; set; }
+    public int AdultsCapacity { get; set; }
+    public int ChildrenCapacity { get; set; }
+}
