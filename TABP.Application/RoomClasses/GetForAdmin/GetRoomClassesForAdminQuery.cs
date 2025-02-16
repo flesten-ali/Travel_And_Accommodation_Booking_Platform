1d5d7d476886ad257489995d3a9a139676e7c34c
flesten-ali <fbada858@gmail.com>
2025-02-16 14:10:54 +0200

    Added GetRoomClassesForAdminQuery and GetRoomClassesForAdminQueryHandler to handle getting room classes for admin

---
+++ TABP.Application/RoomClasses/GetForAdmin/GetRoomClassesForAdminQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using TABP.Domain.Models;
+
+namespace TABP.Application.RoomClasses.GetForAdmin;
+public class GetRoomClassesForAdminQuery : IRequest<PaginatedList<RoomClassForAdminResponse>>
+{
+    public int PageSize { get; set; }
+    public int PageNumber { get; set; }
+}
---
+++ TABP.Application/RoomClasses/GetForAdmin/GetRoomClassesForAdminQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MediatR;
+using TABP.Domain.Interfaces.Persistence.Repositories;
+using TABP.Domain.Models;
+
+namespace TABP.Application.RoomClasses.GetForAdmin;
+public class GetRoomClassesForAdminQueryHandler
+    : IRequestHandler<GetRoomClassesForAdminQuery, PaginatedList<RoomClassForAdminResponse>>
+{
+    private readonly IRoomClassRepository _roomClassRepository;
+    private readonly IMapper _mapper;
+
+    public GetRoomClassesForAdminQueryHandler(IRoomClassRepository roomClassRepository, IMapper mapper)
+    {
+        _roomClassRepository = roomClassRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<PaginatedList<RoomClassForAdminResponse>> Handle(
+        GetRoomClassesForAdminQuery request,
+        CancellationToken cancellationToken)
+    {
+        var roomClasses = await _roomClassRepository.GetRoomClassesForAdminAsync(request.PageSize, request.PageNumber);
+
+        return _mapper.Map<PaginatedList<RoomClassForAdminResponse>>(roomClasses);
+    }
+}
+//async
+//order parameter
+//centrilize validations
