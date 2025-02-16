2451b4da30401feaaa38f91d8b3a1bc40862a167
flesten-ali <fbada858@gmail.com>
2025-02-16 23:18:29 +0200

    -Update GetHotelsForAdminQuery and GetHotelsForAdminQueryHandler to use PaginationParameters -Add BuildSort logic

--- TABP.Application/Hotels/Queries/GetForAdmin/GetHotelsForAdminQuery.cs
+++ TABP.Application/Hotels/Queries/GetForAdmin/GetHotelsForAdminQuery.cs
@@ -4,6 +4,5 @@ using TABP.Domain.Models;
 namespace TABP.Application.Hotels.Queries.GetForAdmin;
 public class GetHotelsForAdminQuery : IRequest<PaginatedList<HotelForAdminResponse>>
 {
-    public int PageSize { get; set; }
-    public int PageNumber { get; set; }
+    public PaginationParameters PaginationParameters { get; set; }
 }
--- TABP.Application/Hotels/Queries/GetForAdmin/GetHotelsForAdminQueryHandler.cs
+++ TABP.Application/Hotels/Queries/GetForAdmin/GetHotelsForAdminQueryHandler.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using MediatR;
+using TABP.Domain.Entities;
+using TABP.Domain.Enums;
 using TABP.Domain.Interfaces.Persistence.Repositories;
 using TABP.Domain.Models;
 
@@ -17,8 +19,29 @@ public class GetHotelsForAdminQueryHandler
     }
     public async Task<PaginatedList<HotelForAdminResponse>> Handle(GetHotelsForAdminQuery request, CancellationToken cancellationToken)
     {
-        var hotels = await _hotelRepository.GetHotelsForAdminAsync(request.PageSize, request.PageNumber);
+        var orderBy = BuildSort(request.PaginationParameters);
+        var hotels = await _hotelRepository.GetHotelsForAdminAsync(
+            orderBy,
+            request.PaginationParameters.PageSize,
+            request.PaginationParameters.PageNumber);
 
         return _mapper.Map<PaginatedList<HotelForAdminResponse>>(hotels);
+    }
+
+    private static Func<IQueryable<Hotel>, IOrderedQueryable<Hotel>> BuildSort(PaginationParameters paginationParameters)
+    {
+        var isDescending = paginationParameters.SortOrder == SortOrder.Descending;
+        return paginationParameters.OrderColumn?.ToLower().Trim() switch
+        {
+            "name" => isDescending
+            ? hotels => hotels.OrderByDescending(x => x.Name)
+            : hotels => hotels.OrderBy(x => x.Name),
+
+            "starrating" => isDescending
+            ? hotels => hotels.OrderByDescending(x => x.Rate)
+            : hotels => hotels.OrderBy(x => x.Rate),
+
+            _ => hotels => hotels.OrderBy(x => x.Id),
+        };
     }
 }
