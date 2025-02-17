a03681471c04ffa3880ea197ec47c2a3a65f9dbb
flesten-ali <fbada858@gmail.com>
2025-02-17 23:59:50 +0200

    Remove unused exception

--- TABP.Domain/Constants/ExceptionMessages/EntityExceptionMessages.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace TABP.Domain.Constants.ExceptionMessages;
-public class EntityExceptionMessages
-{
-    public const string NotFound = "The specified entity could not be found. Please verify the entity details.";
-}
