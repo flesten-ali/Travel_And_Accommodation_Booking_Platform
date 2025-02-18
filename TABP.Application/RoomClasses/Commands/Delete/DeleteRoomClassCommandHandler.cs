8216c2256fa24d02ea00f2bb9bc4d3281c054703
flesten-ali <fbada858@gmail.com>
2025-02-18 19:27:26 +0200

    Update DeleteRoomClassCommandHandler logic

--- TABP.Application/RoomClasses/Commands/Delete/DeleteRoomClassCommandHandler.cs
+++ TABP.Application/RoomClasses/Commands/Delete/DeleteRoomClassCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using TABP.Domain.Constants.ExceptionMessages;
+using TABP.Domain.Enums;
 using TABP.Domain.Exceptions;
 using TABP.Domain.Interfaces.Persistence;
 using TABP.Domain.Interfaces.Persistence.Repositories;
@@ -9,14 +10,19 @@ public class DeleteRoomClassCommandHandler : IRequestHandler<DeleteRoomClassComm
 {
     private readonly IRoomClassRepository _roomClassRepository;
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IRoomRepository _roomRepository;
+    private readonly IImageRepository _imageRepository;
 
     public DeleteRoomClassCommandHandler(
         IRoomClassRepository roomClassRepository,
         IUnitOfWork unitOfWork,
-        IAmenityRepository amenityRepository)
+        IRoomRepository roomRepository,
+        IImageRepository imageRepository)
     {
         _roomClassRepository = roomClassRepository;
         _unitOfWork = unitOfWork;
+        _roomRepository = roomRepository;
+        _imageRepository = imageRepository;
     }
 
     public async Task<Unit> Handle(DeleteRoomClassCommand request, CancellationToken cancellationToken = default)
@@ -26,9 +32,23 @@ public class DeleteRoomClassCommandHandler : IRequestHandler<DeleteRoomClassComm
             throw new NotFoundException(RoomClassExceptionMessages.NotFound);
         }
 
-        _roomClassRepository.Delete(request.Id);
-        await _unitOfWork.SaveChangesAsync(cancellationToken);
+        if (await _roomRepository.ExistsAsync(r => r.RoomClassId == request.Id, cancellationToken))
+        {
+            throw new NotFoundException(RoomClassExceptionMessages.EntityInUseForRooms);
+        }
 
+        await _unitOfWork.BeginTransactionAsync(cancellationToken);
+        try
+        {
+            _roomClassRepository.Delete(request.Id);
+            await _imageRepository.DeleteByIdAsync(request.Id, ImageType.Gallery, cancellationToken);
+            await _unitOfWork.CommitAsync(cancellationToken);
+        }
+        catch
+        {
+            await _unitOfWork.RollbackAsync(cancellationToken);
+            throw;
+        }
         return Unit.Value;
     }
 }
--- TABP.Domain/Constants/ExceptionMessages/RoomClassExceptionMessages.cs
+++ TABP.Domain/Constants/ExceptionMessages/RoomClassExceptionMessages.cs
@@ -3,4 +3,12 @@
 public static class RoomClassExceptionMessages
 {
     public const string NotFound = "The specified room class could not be found. Please verify the room class details.";
+    public const string EntityInUseForRooms = """
+                                                   The room class cannot be deleted because it has dependent rooms.
+                                                   Please remove or reassign the dependent rooms before deleting the room class.
+                                              """;
+    //public const string EntityInUseForCartItems = """
+    //                                               The room class cannot be deleted because it has dependent cart items.
+    //                                               Please remove or reassign the dependent cart items before deleting the room class.
+    //                                              """;
 }
