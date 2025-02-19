16420a446a9227492d16eb14a79245bd41be5711
flesten-ali <fbada858@gmail.com>
2025-02-19 18:38:17 +0200

    Rename AddToCart to Create

---
+++ TABP.Application/CartItems/Commands/Create/CreateCartItemCommand.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace TABP.Application.CartItems.Commands.Create;
+public class CreateCartItemCommand : IRequest
+{
+    public Guid UserId { get; set; }
+    public Guid RoomClassId { get; set; }
+}
---
+++ TABP.Application/CartItems/Commands/Create/CreateCartItemCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using MediatR;
+using TABP.Domain.Constants.ExceptionMessages;
+using TABP.Domain.Entities;
+using TABP.Domain.Exceptions;
+using TABP.Domain.Interfaces.Persistence;
+using TABP.Domain.Interfaces.Persistence.Repositories;
+
+namespace TABP.Application.CartItems.Commands.Create;
+public class CreateCartItemCommandHandler : IRequestHandler<CreateCartItemCommand>
+{
+    private readonly ICartItemRepository _cartItemRepository;
+    private readonly IMapper _mapper;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IRoomClassRepository _roomClassRepository;
+    private readonly IUserRepository _userRepository;
+
+    public CreateCartItemCommandHandler(
+        ICartItemRepository cartItemRepository,
+        IMapper mapper,
+        IUnitOfWork unitOfWork,
+        IRoomClassRepository roomClassRepository,
+        IUserRepository userRepository)
+    {
+        _cartItemRepository = cartItemRepository;
+        _mapper = mapper;
+        _unitOfWork = unitOfWork;
+        _roomClassRepository = roomClassRepository;
+        _userRepository = userRepository;
+    }
+
+    public async Task<Unit> Handle(CreateCartItemCommand request, CancellationToken cancellationToken = default)
+    {
+        if (!await _userRepository.ExistsAsync(x => x.Id == request.UserId, cancellationToken))
+        {
+            throw new NotFoundException(UserExceptionMessages.NotFound);
+        }
+
+        if (!await _roomClassRepository.ExistsAsync(x => x.Id == request.RoomClassId, cancellationToken))
+        {
+            throw new NotFoundException(RoomClassExceptionMessages.NotFound);
+        }
+
+        var cartItem = _mapper.Map<CartItem>(request);
+
+        await _cartItemRepository.CreateAsync(cartItem, cancellationToken);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Unit.Value;
+    }
+}
