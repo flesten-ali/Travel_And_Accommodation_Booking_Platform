c6c3b6c0e395e882695f74e3ddfa99b6f1d1825f
flesten-ali <fbada858@gmail.com>
2025-02-20 20:11:24 +0200

    Updated HotelReviewsQueryReponse to include `CreatedDate` and `UpdatedDate`

--- TABP.Application/Reviews/Queries/GetForHotel/HotelReviewsQueryReponse.cs
+++ TABP.Application/Reviews/Queries/GetForHotel/HotelReviewsQueryReponse.cs
@@ -1,8 +1,10 @@
 ﻿namespace TABP.Application.Reviews.Queries.GetForHotel;
 public class HotelReviewsQueryReponse
 {
+    public Guid Id { get; set; }
     public string Comment { get; set; }
     public int Rate { get; set; }
-    public DateTime CreatedAt { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
     public string ReviwerName { get; set; }
 }
