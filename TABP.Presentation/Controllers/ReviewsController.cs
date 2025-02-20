277fb95ab3d348656c30407244ec82afcbb43466
flesten-ali <fbada858@gmail.com>
2025-02-20 17:06:05 +0200

    Update pagination header name

--- TABP.Presentation/Controllers/ReviewsController.cs
+++ TABP.Presentation/Controllers/ReviewsController.cs
@@ -38,7 +38,7 @@ public class ReviewsController(IMediator mediator, IMapper mapper) : ControllerB
 
         var reviews = await _mediator.Send(query, cancellationToken);
 
-        Response.Headers.Append("X-Pagination",
+        Response.Headers.Append("x-pagination",
             JsonSerializer.Serialize(reviews.PaginationMetaData));
 
         return Ok(reviews.Items);
