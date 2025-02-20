7c414781f5afe2fe321685217ea8d71d1c179f33
flesten-ali <fbada858@gmail.com>
2025-02-20 17:07:44 +0200

    Added ClaimPrincipalExtensions to hander getting user id from authenticated jwt claims

---
+++ TABP.Presentation/Extensions/ClaimPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace TABP.Presentation.Extensions;
+public static class ClaimPrincipalExtensions
+{
+    public static Guid GetUserId(this ClaimsPrincipal? principal)
+    {
+        string userId = principal.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        return Guid.TryParse(userId, out var result) ? result : throw new ArgumentNullException();
+    }
+}
