aeb439ac5fc1ea0befd65c70c406879b5a7b8c37
flesten-ali <fbada858@gmail.com>
2025-02-20 19:14:55 +0200

    Remove un necessary `virtual`

--- TABP.Domain/Common/AuditEntity.cs
+++ TABP.Domain/Common/AuditEntity.cs
@@ -1,6 +1,6 @@
 ﻿namespace TABP.Domain.Common;
 public abstract class AuditEntity<Tkey> : EntityBase<Tkey>, IAuditEntity<Tkey>
 {
-    public virtual DateTime CreatedDate { get; set; }
-    public virtual DateTime? UpdatedDate { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
 }
