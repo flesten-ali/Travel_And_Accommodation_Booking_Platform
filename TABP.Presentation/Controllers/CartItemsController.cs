9f453c48f11a1a24ddddbe26bbdf1051fe6a0221
flesten-ali <fbada858@gmail.com>
2025-02-20 17:02:42 +0200

    feat(cart-items): refactor cart items management and enhance user authentication
    - Replaced direct userId parameter with User.GetUserId() for retrieving authenticated user's ID
    - Refactored CreateCartItem method to use CreateCartItemRequest DTO
    - Updated DeleteCartItem method to simplify user authentication
    - Simplified route to api/user/cart-items for clarity and consistency

--- TABP.Presentation/Controllers/CartItemsController.cs
+++ TABP.Presentation/Controllers/CartItemsController.cs
@@ -10,12 +10,13 @@ using TABP.Application.CartItems.Commands.Delete;
 using TABP.Application.CartItems.Queries.GetAll;
 using TABP.Domain.Constants;
 using TABP.Presentation.DTOs.CartItem;
+using TABP.Presentation.Extensions;
 namespace TABP.Presentation.Controllers;
 
-[Route("api/users/{userId:guid}/cart-items")]
+[Route("api/user/cart-items")]
 [ApiController]
 [Authorize(Roles = Roles.Guest)]
-[SwaggerTag("Manage shopping cart items for guests.")]  
+[SwaggerTag("Manage shopping cart items for guests.")]
 public class CartItemsController(IMediator mediator, IMapper mapper) : ControllerBase
 {
     private readonly IMediator _mediator = mediator;
@@ -31,15 +32,11 @@ public class CartItemsController(IMediator mediator, IMapper mapper) : Controlle
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<IActionResult> CreateCartItem(
-        Guid userId,
-        Guid roomClassId,
+        CreateCartItemRequest request,
         CancellationToken cancellationToken)
     {
-        var command = new CreateCartItemCommand
-        {
-            UserId = userId,
-            RoomClassId = roomClassId
-        };
+        var command = _mapper.Map<CreateCartItemCommand>(request);
+        command.UserId = User.GetUserId();
 
         await _mediator.Send(command, cancellationToken);
 
@@ -56,12 +53,12 @@ public class CartItemsController(IMediator mediator, IMapper mapper) : Controlle
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
-    public async Task<IActionResult> DeleteCartItem(Guid userId, Guid id, CancellationToken cancellationToken)
+    public async Task<IActionResult> DeleteCartItem(Guid id, CancellationToken cancellationToken)
     {
         var command = new DeleteCartItemCommand
         {
             CartId = id,
-            UserId = userId,
+            UserId = User.GetUserId(),
         };
 
         await _mediator.Send(command, cancellationToken);
@@ -80,12 +77,11 @@ public class CartItemsController(IMediator mediator, IMapper mapper) : Controlle
     [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetCartItems(
-        Guid userId,
         [FromQuery] GetCartItemsRequest request,
         CancellationToken cancellationToken)
     {
         var query = _mapper.Map<GetCartItemsQuery>(request);
-        query.UserId = userId;
+        query.UserId = User.GetUserId();
 
         var cartItems = await _mediator.Send(query, cancellationToken);
 
