da3441955ae9e8aad34a2dd7c544eaacf7492398
flesten-ali <fbada858@gmail.com>
2025-02-20 20:15:50 +0200

    Update GetUserId consider nullability

--- TABP.Presentation/Extensions/ClaimPrincipalExtensions.cs
+++ TABP.Presentation/Extensions/ClaimPrincipalExtensions.cs
@@ -5,7 +5,7 @@ public static class ClaimPrincipalExtensions
 {
     public static Guid GetUserId(this ClaimsPrincipal? principal)
     {
-        string userId = principal.FindFirstValue(ClaimTypes.NameIdentifier);
+        string? userId = principal?.FindFirstValue(ClaimTypes.NameIdentifier);
 
         return Guid.TryParse(userId, out var result) ? result : throw new ArgumentNullException();
     }
