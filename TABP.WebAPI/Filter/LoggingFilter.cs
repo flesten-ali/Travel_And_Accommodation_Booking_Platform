b9ff0a26162630ad0aae3359b0fb1ad49c40f03d
flesten-ali <fbada858@gmail.com>
2025-02-22 18:28:37 +0200

    Added Logging filter

---
+++ TABP.WebAPI/Filter/LoggingFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Text.Json;
+
+namespace TABP.WebAPI.Filter;
+
+public class LoggingFilter(ILogger<LoggingFilter> logger) : IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        var body = JsonSerializer.Serialize(context.ActionArguments.ToDictionary(k => k.Key, v => v.Value.ToString()));
+
+        logger.LogInformation(
+            "Incoming Request: {Method} {Path}, Body: {body}",
+            context.HttpContext.Request.Method,
+            context.HttpContext.Request.Path,
+            body);
+
+        await next();
+
+        logger.LogInformation(
+            "Response: {Path} returned {StatusCode}",
+            context.HttpContext.Request.Path,
+            context.HttpContext.Response.StatusCode);
+    }
+}
