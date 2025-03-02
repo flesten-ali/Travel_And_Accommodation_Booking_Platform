1faa972cdcf6f4e510368645be9f71819508cb5f
flesten-ali <fbada858@gmail.com>
2025-03-02 21:09:15 +0200

    Added Seed Admin Migration

---
+++ TABP.Infrastructure/Persistence/Migrations/20250302185707_AddAdminUser.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using TABP.Domain.Constants;
+
+#nullable disable
+
+namespace TABP.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAdminUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                 table: "Users",
+                 columns: ["Id", "UserName", "Email", "PasswordHash", "Role"],
+                 values:
+                 [
+                Guid.NewGuid(),
+                "admin",
+                "admin@example.com",
+                "AQAAAAIAAYagAAAAEKEkygIjOMgtLQ8PuQx8r+UZigqwullsU7s1UZEIpjya4FGkSO0foKKlxC2VhTmkmA==",
+                Roles.Admin
+                 ]
+             );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+            table: "Users",
+            keyColumn: "UserName",
+            keyValue: "admin"
+            );
+        }
+    }
+}
